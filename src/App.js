c42d4800703854fa99553c485b7f0bc919a94b2b
Irfan Khan <khanzz_irfan@hotmail.com>
2022-10-05 13:05:56 +1100

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Timer from "./container/Timer";
+import Timer from "./containers//Timer";
 
 function App() {
     return (
         <div>
-            <h1 className="text-5xl md:text-4xl text-center font-thin pt-8 px-6">Redux Stopwatch</h1>
+            <h1 className="text-5xl md:text-4xl text-center font-thin pt-8 px-6">Stopwatch Timer</h1>
             <Timer />
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import timerReducer from "./store/reducers/timer";
-import lapsReducer from "./store/reducers/laps";
-
-const rootReducer = combineReducers({
-  tmr: timerReducer,
-  lpr: lapsReducer,
-});
-
-const store = createStore(rootReducer);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <App />, document.getElementById('root')
 );
